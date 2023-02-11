90067813e94b09aa9dd6d689a9ef3b23824222ab
anas jaidi <anas.jaidi@icloud.com>
2023-02-11 19:40:25 +0100

    template class

--- components/templates.cpp
+++ components/templates.cpp
@@ -5,11 +5,23 @@ template<typename T>
 void Print(T obj) {
   std::cout << obj << std::endl;
 }
+template<typename T, int N>
+class Array {
+  private:
+    T arr[N];
+  public:
+  T getN() {
+    return N;
+  }
+};
 
 int main()
 {
-  Print<float>(1.2f);
-  Print<int>(1);
-  Print<std::string>("anas");
+  // Print<float>(1.2f);
+  // Print(1);
+  // Print<std::string>("anas");
+
+  Array<int, 10> arr;
+  std::cout << arr.getN() << std::endl;
   return 0;
 }
