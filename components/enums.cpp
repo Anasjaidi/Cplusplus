b8ae08a9c980cc01aeb1254078697dad19258710
anas jaidi <anas.jaidi@icloud.com>
2023-02-13 00:19:33 +0100

    add enums in c++

---
+++ components/enums.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+/**
+ * enums just a named values
+*/
+enum Dir {
+  A,
+  B,
+  C
+};
+
+int main(int argc, char const *argv[])
+{
+  std::cout << A << B << C << std::endl;
+  return 0;
+}
--- components/static.c++
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-/**
- * c++ hase two meaning of class and behaviour depends
- * inside class(struct): the static X gone be shared with all of instances 
- * outside class(struct): 
- * **/
